600bdb0a42c62a0b53858eca3e05614f2029eca7
Allie Robinson <allie-rae@Allies-MacBook.local>
2019-11-10 00:36:56 -0600

    Display and Dashboard components created. Balls and strikes showing on screen. Strike, ball, foul, and hit button rendering and 100% functional.

---
+++ baseball-app/src/components/Display.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Display = (props) => {
+
+    return (
+        <>
+            <div>
+                <h2>Balls:</h2>
+                {props.value.ball}
+            </div>
+            <div>
+                <h2>Strikes:</h2>
+                {props.value.strike}
+            </div>
+        </>
+    )
+}
+
+export default Display; 
